2c24cf2994b3dfe8f0ab3fcaf960dfd81fae1aa7
web-developer-11 <77283008+Vikas-tiwari-developer@users.noreply.github.com>
2023-02-10 00:16:19 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/vishpa/botgpt/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.vishpa.botgpt
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:botgpt/chat_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return  MaterialApp(
+      title: 'Chat GPT with flutter',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+        useMaterial3: true,
+      ),
+      home: const ChatScreen(),
+    );
+  }
+}
